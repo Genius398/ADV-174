7de9dedd31b330cdde05a27717761241f6dafc4b
Anantjit <anantjitchander@gmail.com>
2022-02-08 17:46:47 +0530

    Add files via upload

---
+++ ADV-C175-student.py
@@ -0,0 +1,36 @@
+ from tkinter import *
+ import speedtest
+ 
+ root=Tk()
+ root.config(bg="#dee9f1")
+ root.title("Internet Speed Test")
+ 
+ label=Label(root, text="Internet Speed Test", font=("Lucida Sansa Unicode", 20, "bold", "italic"), fg="#5D6D7E", bg="#dee8f1")
+ label.place(relx=0.5, rely=0.1, anchor=CENTER)
+ 
+ label_download=Label(root, text="Download Speed", font=("Segoe Print", 18, "bold"), fg="#1E8449",bg="#dee8f1")
+ label_download.place(relx=0.25, rely=0.5, anchor=CENTER)
+ 
+ label_upload=Label(root, text="Upload Speed", font=("Segoe Print", 18, "bold"), fg="#1E8449",bg="#dee8f1")
+ label_upload.place(relx=0.75, rely=0.5, anchor=CENTER)
+ 
+ label_upload_speed=Label(root, font=("Yu Gothic Light", 14, "bold"), bg="#dee8f1")
+ label_upload_speed.place(relx=0.75, rely=0.6, anchor=CENTER)
+ 
+ label_download_speed=Label(root, font=("Yu Gothic Light", 14, "bold"), bg="#dee8f1")
+ label_download_speed.place(relx=0.25, rely=0.6, anchor=CENTER)
+
+ 
+ def speedCheck():
+     st=speedtest.Speedtest()
+     download_Speed = round(st.download()/1000000, 2)
+     label_download_speed['test'] =str(download_speed) + "mbps"
+     
+     upload_Speed = round(st.upload()/1000000, 2)
+     label_upload_speed['test'] =str(upload_speed) + "mbps"
+     
+     
+ btn_doctor = Button(root, text="Check Speed", command=speedCheck, bg="#218796", fg="white", relief = FLAT)
+ btn_doctor.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+ root.mainloop()
